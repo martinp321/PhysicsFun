8e81d7a3fa667d588446486a38a996277891033d
Martin Pogorzelski <martinp321@gmail.com>
2016-12-30 07:34:17 -0500

    Initial Commit

---
+++ Assets/Scripts/PhysicsEngine.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour
+{
+
+    public Vector3 velocityVector; // Average velocity this FixedUpdate()
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+
+        transform.position += velocityVector * Time.deltaTime;
+    }
+}
